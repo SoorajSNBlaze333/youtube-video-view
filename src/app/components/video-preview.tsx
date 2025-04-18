94f9a11e015b203ab364bb6e04df07c0de2e05f0
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2025-04-18 12:42:47 -0500

    feat: updated view preview bg

--- src/app/components/video-preview.tsx
+++ src/app/components/video-preview.tsx
@@ -1,7 +1,7 @@
 export default function VideoPreview({ video }: { video: string }) {
   return (
     <div className="grid grid-cols-11 gap-2 mb-2">
-      <div className="col-span-5 rounded-lg bg-slate-500 aspect-video" />
+      <div className="col-span-5 rounded-lg bg-slate-300 aspect-video" />
       <div className="col=span-6 flex flex-col justify-start">{video}</div>
     </div>
   );
